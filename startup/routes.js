2e30d4b570d377cd4e5967b1920a51a409d2f2b2
Arun Gautam <arunraj1735169@gmail.com>
2019-11-25 23:06:58 +0530

    modified for heroku

--- startup/routes.js
+++ startup/routes.js
@@ -3,8 +3,7 @@ const date = require('../routes/isoDATE');
 const cors = require("cors");
 
 module.exports = (app) => {
-    app.use(express.json());
-
+    app.use(express.json())
     app.use(cors());
     
     app.use('/api/date',date);
